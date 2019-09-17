cee4a2f9fdd4ab6e3b5aa2da1adec65bcb0e2ae5
Aan Niraj Patel <37667704+aannirajpatel@users.noreply.github.com>
2019-09-16 23:27:12 -0400

    Update settings.php

--- login/settings.php
+++ login/settings.php
@@ -1,10 +1,10 @@
 <?php
 
 /* Google App Client Id */
-define('CLIENT_ID', '914497376571-4pa8o5i6valmsti6v1uvvsr3n6vh0muo.apps.googleusercontent.com');
+define('CLIENT_ID', '');
 
 /* Google App Client Secret */
-define('CLIENT_SECRET', '-VAokXQF8u-gVL1UkWypuJo7');
+define('CLIENT_SECRET', '');
 
 /* Google App Redirect Url */
 define('CLIENT_REDIRECT_URL', 'http://localhost/orion/login/oauth2callback.php');
